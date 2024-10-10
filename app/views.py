fc553b60bdfba4d1104303751cd7ad4ac26d31e5
riekoqq <your.email@example.com>
2024-10-10 10:25:02 +0800

    ahhh patient profile auth

--- app/views.py
+++ app/views.py
@@ -4,7 +4,19 @@ from django.shortcuts import render
 
 @login_required
 def home_page(request):
-    return render(request, "app/index.html")
+    user = request.user
+    context = {}
+
+    if user.is_staff:
+        pass
+    else:
+        patient = user.patient_info
+        medical_records = patient.medical_records.all()
+
+        context["patient"] = patient
+        context["medical_records"] = medical_records
+
+    return render(request, "app/index.html", context)
 
 
 @login_required
