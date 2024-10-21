119cd0366dce48af4033cb18102c57bd406b503e
Justin Rieko Tolentino <jstolentino2@student.hau.edu.ph>
2024-10-21 09:22:18 +0800

    deploy ulet

--- hms/settings/production.py
+++ hms/settings/production.py
@@ -75,6 +75,11 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+DATABASES['default']['CONN_MAX_AGE'] = 500
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -137,3 +142,4 @@ CSRF_COOKIE_SECURE              = True
 SECURE_HSTS_INCLUDE_SUBDOMAINS  = True
 SECURE_HSTS_SECONDS             = 1000000
 SECURE_FRAME_DENY               = True
+
